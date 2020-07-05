4a01125ba4abe38b16396d7767fbba95d4d0e487
wanda agustiansyah r <wandatamvan@gmail.com>
2020-07-05 16:44:06 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "KueBastard", quotes: "uuumm, hai caa. Selamat Ulang Tahun yaa, bagiku berapa nya gak penting yang penting kamu bersamaku UwU. sehat selalu, panjang umur ya caca." },
-    { author: "KueBastard", quotes: "haii shakira, selamat ulang tahun. walau kamu merasa kalo kamu gk bisa ada selalu buat ku tapi aku sayang sama kamu selalu ada kapan pun" },
-    { author: "KueBastard", quotes: "caaa selamat ulang tahunn, aku cuma mau bilang ily caa ily forever :v" },
+    { author: "KueBastard", quotes: "uuumm, hai caa. Selamat Ulang Tahun yaa, kamu sama kayak matahari dan bulan. kemanapun ku pergi pasti kau bersamaku dari sisi yang jauh :D sehat selalu, panjang umur ya caca." },
+    { author: "KueBastard", quotes: "haii shakira, selamat ulang tahun. semoga panjang umur sehat selalu, makin pinter gak kanak kanak lagi. udah bukan bocil lagi kamu hahah" },
+    { author: "KueBastard", quotes: "caaa selamat ulang tahunn, aku cuma mau bilang kalo aku saaaaaaaaaaaaa, ah ntah berat ngatain nya." },
     { author: "KueBastard", quotes: "caca kamu sama martabak sama sama manis, jadi gak usah pakek kueh lagi ya. kan ada kamu.. :v selamat ulang tahun caca!" },
     { author: "KueBastard", quotes: "HBD ANANDA DWI SHAKIRA, I may not with you everyday, but I love you everyday." },
-    { author: "KueBastard", quotes: "I love you. hapy birthday shakira :* " },
-    { author: "KueBastard", quotes: "I'm sorry for loving you." },
+    { author: "KueBastard", quotes: "ca aku mungkin bukan tipe kamu, aku masih mainin game aku dimata kamu kenak anakan cuma itu doank yang kamu tau. itu sih cuma gaya aku aja dalam menikmati hidup. aku menolak tua tuk bersama mu... hapy birthday shakira :* " },
+    { author: "KueBastard", quotes: "Happy Birthday!! I'm sorry for loving you. :(" },
 ];
